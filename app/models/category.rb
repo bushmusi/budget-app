a2d30d475e2adc07a2850943c5704783f046cab7
bushmusi <bushera.mestofa@gmail.com>
2022-07-06 17:15:32 +0300

    Define category and payment relation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
-  belongs_to :user, class_name: 'user', foreign_key: 'author_id'
+  belongs_to :user
+  has_many :category_payments
+  has_many :payments, through: :category_payments
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 50 }
+  validates :icon, presence: true
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,8 @@
 class Payment < ApplicationRecord
-  belongs_to :user, class_name: 'user', foreign_key: 'author_id'
+  belongs_to :user
+  has_many :category_payments
+  has_many :categories, through: :category_payments
+
+  validates_presence_of :name, :amount
+  validates :amount, numericality: { greater_than_or_equal_to: 1 }
 end
