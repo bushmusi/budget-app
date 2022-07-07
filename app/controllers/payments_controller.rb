384f8a5d366eee828896e7396429fc43516b662d
bushmusi <bushera.mestofa@gmail.com>
2022-07-07 17:21:16 +0300

    Payment new create

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,32 @@
 class PaymentsController < ApplicationController
+  before_action :current_category
   def index
   end
 
   def new
+    @payment = Payment.new
+  end
+
+  def create
+    @payment = Payment.new(paymement_params)
+    @payment.user = current_user
+    respond_to do |format|
+      if @payment.save
+        CategoryPayment.create(name: @payment.name, payment_id: @payment.id, category_id: @category.id)
+        format.html { redirect_to category_path(@category), notice: 'Expense added successfully' }
+      else
+        format.html { redirect_to category_path(@category), alert: @payment.errors }
+      end
+    end
+  end
+
+  def current_category
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def paymement_params
+    params.require(:payment).permit(:name, :amount)
   end
 end
