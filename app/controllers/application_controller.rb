0034aeb30d088eeb91e4f8ffc699fec7e8d1a8b1
bushmusi <bushera.mestofa@gmail.com>
2022-07-07 17:20:37 +0300

    Update path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(user)
-    category_index_path(user)
+    category_index_path
   end
 
   protected
