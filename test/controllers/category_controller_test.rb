cde8c71378b4777616ec2231660eccf70d21966f
bushmusi <bushera.mestofa@gmail.com>
2022-07-08 12:23:40 +0300

    Add tests

--- test/controllers/category_controller_test.rb
+++ test/controllers/category_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class CategoryControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get category_index_url
     assert_response :success
   end
